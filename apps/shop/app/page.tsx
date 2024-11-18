a24aa4493627e3c41e13f740e39543c6a33f077d
godspeed-03 <satyam2001anand@gmail.com>
2024-11-19 00:47:01 +0530

    feat : integrated shadcn with in ui pakage library

--- apps/shop/app/page.tsx
+++ apps/shop/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { cn } from "@repo/ui/cn";
 
 export default function Home() {
+  const isActive = false;
+
   return (
-<div>shop</div>
-  );
+    <div className={cn("bg-blue-500", isActive && "text-white", "p-4")}>
+      shop
+    </div>
+  )
 }
--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -3,5 +3,7 @@ const config = require("../tailwind-config/tailwind.config.cjs");
 
 module.exports ={
     ...config,
-    content : ["./src/**/*.tsx"],
+    content : ["./src/**/*.tsx", "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"],
 }
